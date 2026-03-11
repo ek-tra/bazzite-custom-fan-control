cb030f3dfaeff7f60e934659239bed20d5a29f30
ek-tra <ek_trading@gmx.de>
2026-03-11 22:17:03 +0100

    Update build.sh
    Korrigiertes Build-Skript (Kernel-Headers + set -e)

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -e  # Stoppt bei jedem Fehler sofort
+set -e
 
 echo "=== EC-Fan-Control für Bosgame M5 wird installiert ==="
 
-# Alle notwendigen Pakete auf einmal installieren
+# Wichtige Abhängigkeiten + Kernel-Headers für den aktuellen Kernel
 dnf install -y git make gcc kernel-devel kernel-headers dnf-plugins-core
 
 # Treiber herunterladen und bauen
@@ -15,7 +15,7 @@ make -j$(nproc)
 mkdir -p /usr/lib/modules/$(uname -r)/extra
 cp ec_su_axb35.ko /usr/lib/modules/$(uname -r)/extra/
 
-# Automatisch beim Boot laden
+# Automatisch laden beim Boot
 echo "ec_su_axb35" > /etc/modules-load.d/ec-su_axb35.conf
 
 # Aufräumen
